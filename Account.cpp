3098d8d5f576f522569e897d6eb2eaa5480911a3
TaiVanNgo <vantaingo.056@gmail.com>
2023-12-16 11:33:20 +0700

    Add account class

---
+++ Account.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+class Account{
+private:
+    string user_name;
+    string email;
+    string pass_word;
+public:
+    //CONSTRUCTOR
+    Account(string user_name = "", string email = "", string pass_word = "")
+            : user_name(user_name), email(email), pass_word(pass_word){}
+
+    //GETTER & SETTER
+    string getUsername(){return this->user_name;}
+    string getPassword(){return this->pass_word;}
+    string getEmail(){return this->email;}
+
+    void setUsername(string user_name){
+        this->user_name = user_name;
+    }
+
+    void setPassword(string pass_word){
+        this->pass_word = pass_word;
+    }
+};
