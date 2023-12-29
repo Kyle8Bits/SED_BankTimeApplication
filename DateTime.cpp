716b4a714e8c9acf2eab105ca9dc305fbd49afdb
Kyle4Bits <s3974876@rmit.edu.vn>
2023-12-29 16:16:20 +0700

    comment

--- DateTime.cpp
+++ DateTime.cpp
@@ -1,3 +1,5 @@
+//CHƯA CHẮC LÀ SẼ XÀI TỚI CÁI NÀY, ĐỂ ĐÂY DỰ PHÒNG//
+
 #include <iostream>
 #include <ctime>
 
@@ -5,10 +7,9 @@ class DateTime {
 public:
     // Constructor
     DateTime() {
-        update();  // Initialize the object with the current date and time
+        update();
     }
 
-    // Function to update the date and time
     void update() {
         std::time_t t = std::time(0);
         std::tm* now = std::localtime(&t);
@@ -18,12 +19,10 @@ public:
         dayOfWeek = now->tm_wday;
     }
 
-    // Function to print the current date
     void printDate() const {
         std::cout << "Current Date: " << year << '-' << month << '-' << day << std::endl;
     }
-
-    // Function to print the day of the week
+    
     void printDayOfWeek() const {
         const char* days[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
         std::cout << "Day of the Week: " << days[dayOfWeek] << std::endl;
